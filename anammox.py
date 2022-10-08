92a1928e5f5fec4cb5d90015c0d8e5b6c317568e
ouchen-bio <85400693+ouchen-bio@users.noreply.github.com>
2022-10-08 18:30:36 +0100

    Update anammox.py

--- anammox.py
+++ anammox.py
@@ -93,7 +93,7 @@ if select=='Overview':
     st.write('The following web-integrated application enables 3D visualization of anammox key enzymes. This app is powred by Py3Dmol.')
     #proteins 3d structure
     st.markdown("***")
-    cly,clx=st.columns([2.67,.745])
+    cly,clx=st.columns([2.665,.745])
     with clx:
         prot_name={'Hydrazine synthase':'5C2V','Hydrazine dehydrogenase':'6HIF','Cd1 nitrite reductase Nirs':'6TSI','Nitrite oxidoreductase':'7b04'}
         protein=st.selectbox('Select Enzyme :',prot_name)
