9e959a43f01f57b75f5977f49ad0e4f0282a3947
ouchen-bio <85400693+ouchen-bio@users.noreply.github.com>
2022-10-08 20:40:36 +0100

    Update anammox.py

--- anammox.py
+++ anammox.py
@@ -108,7 +108,7 @@ if select=='Overview':
         xyzview1=py3Dmol.view(query='pdb:'+prot_name.get(protein))
         xyzview1.setStyle({style:{'color':'spectrum'}})
         xyzview1.setBackgroundColor(b_color)
-        showmol(xyzview1,height=500,width=800)
+        showmol(xyzview1,height=500,width=750)
     st.markdown('***')
     #Physiology
     st.markdown('')
